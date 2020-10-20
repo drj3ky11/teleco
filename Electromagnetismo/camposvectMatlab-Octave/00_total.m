3d63e2c0f10c6ba00a22f67dd5ccdc9759ea4469
Dr.Jekyll <38112807+drj3ky11@users.noreply.github.com>
2020-10-20 13:33:54 +0200

    Create 00_total.m
    Función que engloba al resto

---
+++ Electromagnetismo/camposvectMatlab-Octave/00_total.m
@@ -0,0 +1,10 @@
+function aa1_joseluis()
+  %% Función final que implementa los apartados de la AA1
+  
+  
+  apartado1();
+  apartado2();
+  apartado3();
+  apartado4();
+% Para octave si se puede usar la siguiente línea: 
+%endfunction
